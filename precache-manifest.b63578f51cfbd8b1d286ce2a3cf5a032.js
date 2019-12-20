850095e5bf423018f46aeef448c82f094183515c
Ljupce11 <ljubomir.bojadziev@gmail.com>
2019-12-20 01:04:34 +0100

    Updates

---
+++ precache-manifest.b63578f51cfbd8b1d286ce2a3cf5a032.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9407b58f4daa7600747ff15baf03e47",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "2fa1a9b26155c4740145",
+    "url": "/test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "706f41ffbdf26bf106bf",
+    "url": "/test/static/js/2.39513104.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test/static/js/2.39513104.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2fa1a9b26155c4740145",
+    "url": "/test/static/js/main.a49cd561.chunk.js"
+  },
+  {
+    "revision": "e12a66e52bfda4ada124",
+    "url": "/test/static/js/runtime-main.e2582418.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
