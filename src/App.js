eea01bf0dff0c753ab4443ef59c3ac8df5ad49bc
Ljupce11 <ljubomir.bojadziev@gmail.com>
2019-12-20 01:33:20 +0100

    Replace browserRouter with hashRouter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import Auth from './components/Auth/Auth';
 import Login from './components/Login/Login';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Route path="/" exact component={Auth} />
       <Route path="/login" component={Login} />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
